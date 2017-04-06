4e37062a5990ac75648df2b51a4b01bae6db2856
Jan Janiczek <jan.janiczek@yahoo.com>
2017-04-06 21:22:36 +0200

    changed the back link into a button and moved the col-sm-4 to an enclosing div

--- hypergolic/catalog/static/js/hypergol.js
+++ hypergolic/catalog/static/js/hypergol.js
@@ -1,5 +1,4 @@
 $(function(){
-    $('form').addClass('col-sm-4');
     $('form').find('select, [type=text], [type=password], [type=email],\
                     [type=url], [type=search], [type=datetime-local], textarea,\
                     [type=date], [type=month], [type=time], [type=week],\
@@ -9,5 +8,5 @@ $(function(){
     $('form').find('[type=radio], [type=checkbox]').addClass('form-check-input');
     $('form').find('[type=radio], [type=checkbox]').parent('div, p').addClass('form-check');
     $('form').find('[type=radio], [type=checkbox]').sibling('label').addClass('form-check-label');
-    $('form').find('[type=submit]').addClass('btn btn-primary');
+    $('form').children('[type=submit]').addClass('btn btn-success');
 });
