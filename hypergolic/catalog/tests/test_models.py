600a83b37fb7f7a02965dbd105af82b148f85c4a
Jan Janiczek <jan.janiczek@yahoo.com>
2017-04-11 16:48:33 +0200

    first batch of proper tests, resolved the 'auth_user does not exist' problem

--- hypergolic/catalog/tests/test_models.py
+++ hypergolic/catalog/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import User, AnonymousUser
 from catalog.models import Role
 
 
@@ -6,37 +7,39 @@ class RoleModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        cls.myuser = User.objects.create_user(username='testuser',
+                                              password='ytrewq')
         Role.objects.create(name='aaa', description='loremipsum', sources='xx')
 
     def test_verbose_name(self):
-        tester = Role.objects.get(id=1)
+        tester = Role.objects.get(name='aaa')
         self.assertEquals(tester._meta.verbose_name, 'Engine role')
 
     def test_verbose_plural(self):
-        tester = Role.objects.get(id=1)
+        tester = Role.objects.get(name='aaa')
         self.assertEquals(tester._meta.verbose_name_plural, 'Engine roles')
 
     def test_get_absolute_url(self):
-        tester = Role.objects.get(id=1)
-        self.assertEquals(tester.test_get_absolute_url(),
-                          '/catalog/engine_roles/aaa/')
+        tester = Role.objects.get(name='aaa')
+        self.assertEquals(tester.get_absolute_url(),
+                          '/engine_roles/aaa/')
 
     def test_get_create_url(self):
-        tester = Role.objects.get(id=1)
-        self.assertEquals(tester.test_get_create_url(),
-                          '/catalog/engine_roles/create/')
+        tester = Role.objects.get(name='aaa')
+        self.assertEquals(tester.get_create_url(),
+                          '/engine_roles/create/')
 
     def test_get_update_url(self):
-        tester = Role.objects.get(id=1)
-        self.assertEquals(tester.test_get_update_url(),
-                          '/catalog/engine_roles/aaa/update/')
+        tester = Role.objects.get(name='aaa')
+        self.assertEquals(tester.get_update_url(),
+                          '/engine_roles/aaa/update/')
 
     def test_get_delete_url(self):
-        tester = Role.objects.get(id=1)
-        self.assertEquals(tester.test_get_delete_url(),
-                          '/catalog/engine_roles/aaa/delete/')
+        tester = Role.objects.get(name='aaa')
+        self.assertEquals(tester.get_delete_url(),
+                          '/engine_roles/aaa/delete/')
 
     def test_get_list_url(self):
-        tester = Role.objects.get(id=1)
-        self.assertEquals(tester.test_get_list_url(),
-                          '/catalog/engine_roles/')
+        tester = Role.objects.get(name='aaa')
+        self.assertEquals(tester.get_list_url(),
+                          '/engine_roles/')
